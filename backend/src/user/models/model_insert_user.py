e07c9d1017208261015331fb35d113ac216c8d3e
Kaio Barbosa <kaio.barbosa0824@gmail.com>
2026-08-04 21:36:27 -0300

    WIP: now user can add image profile during the criation account

--- backend/src/user/models/model_insert_user.py
+++ backend/src/user/models/model_insert_user.py
@@ -1,13 +1,20 @@
 def user_model_insert(data):
 
-    user_sql = "INSERT INTO users (name_user, lastname_user, tel_number, email, password) VALUES (%s, %s, %s, %s, %s);"
+    user_sql = "INSERT INTO users (name_user, lastname_user, tel_number, email, password, image_profile) VALUES (%s, %s, %s, %s, %s, %s);"
+
+    nome_arquivo = data.get('imageProfile')
+        
+    # Monta a URL usando a string diretamente
+    image_url = f"http://127.0.0.1:5500/backend/src/static/uploads/{nome_arquivo}"
+            
 
     user_values = (
             data.get("name"), 
             data.get("lastname"), 
             data.get("tel_number"), 
             data.get("email"), 
-            data.get("password")
+            data.get("password"),
+            image_url
         )
     
     return user_sql, user_values
--- backend/src/user/routes/user_route_insert.py
+++ backend/src/user/routes/user_route_insert.py
@@ -1,17 +1,24 @@
 from user.usecase.insert_users import insert_new_user
 from user.service.service_insert_user import user_sctructure_insert
 from user.models.model_insert_user import user_model_insert
+from user.util.imageProfileNameSecure import imageSecureName
 
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify
 
 user_route_insert = Blueprint('user_route_insert', __name__)
 
 @user_route_insert.route('/user_insert', methods=['POST'])
 def user_insert():
+    data = request.form.to_dict()
 
-    data = request.get_json()
-    # Process the inserted user data
-
+    if 'imageProfile' not in request.files:
+            return jsonify({"error": "No imageProfile file provided"}), 400
+    
+    arquivo_imagem = request.files['imageProfile']
+        
+    # 4. Passa o arquivo para sua utilidade e adiciona o nome gerado ao dicionário 'data'
+    data['imageProfile'] = imageSecureName(arquivo_imagem)
+        
     user_data = user_sctructure_insert(data)
     user_sql, user_values = user_model_insert(user_data)
     insert_new_user(user_sql, user_values)
@@ -24,5 +31,6 @@ def user_insert():
             "telefone number": user_data.get("tel_number"),
             "email": user_data.get("email"),
             "passsword": user_data.get("password"),
+            "imageProfile": user_data.get("imageProfile")
         }
     }), 201
